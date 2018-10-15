6d712cb6845416e46bdcbcb13885f88f48600932
Magic Len <len@magiclen.org>
2018-10-15 22:43:04 +0800

    initial commit

---
+++ build.rs
@@ -0,0 +1,158 @@
+extern crate bindgen;
+extern crate pkg_config;
+
+use std::env;
+use std::process::Command;
+use std::path::PathBuf;
+use std::collections::HashSet;
+
+const MIN_VERSION: &'static str = "1.0.5";
+const MAX_VERSION: &'static str = "1.1.0";
+
+fn main() {
+    if cfg!(target_os = "freebsd") {
+        env_var_set_default("OPENCC_INCLUDE_DIRS", "/usr/include/opencc");
+        env_var_set_default("OPENCC_LIB_DIRS", "/usr/lib");
+        env_var_set_default("OPENCC_LIBS", "opencc");
+    }
+
+    let lib_dirs = find_opencc_lib_dirs();
+
+    for d in &lib_dirs {
+        if !d.exists() {
+            panic!("OpenCC library directory does not exist: {}", d.to_string_lossy());
+        }
+        println!("cargo:rustc-link-search=native={}", d.to_string_lossy());
+    }
+
+    let include_dirs = find_opencc_include_dirs();
+    for d in &include_dirs {
+        if !d.exists() {
+            panic!("OpenCC include directory does not exist: {}", d.to_string_lossy());
+        }
+        println!("cargo:include={}", d.to_string_lossy());
+    }
+    println!("cargo:rerun-if-env-changed=OPENCC_LIBS");
+
+    let target = env::var("TARGET").unwrap();
+    let libs_env = env::var("OPENCC_LIBS").ok();
+
+    let libs = match libs_env {
+        Some(ref v) => v.split(":").map(|x| x.to_owned()).collect(),
+        None => {
+            if target.contains("windows") {
+                vec!["opencc".to_string()] // TODO: not sure
+            } else if target.contains("freebsd") {
+                vec!["opencc".to_string()]
+            } else {
+                run_pkg_config().libs
+            }
+        }
+    };
+
+    let kind = determine_mode(&lib_dirs, libs.as_slice());
+    for lib in libs.into_iter() {
+        println!("cargo:rustc-link-lib={}={}", kind, lib);
+    }
+
+    println!("cargo:rerun-if-env-changed=OPENCC_DYLIB_STDCPP");
+    let kind = env::var("OPENCC_DYLIB_STDCPP");
+    match kind.as_ref().map(|s| &s[..]) {
+        Ok("0") => (),
+        Ok(_) => println!("cargo:rustc-link-lib=dylib=stdc++"),
+        Err(_) => ()
+    }
+}
+
+fn env_var_set_default(name: &str, value: &str) {
+    if env::var(name).is_err() {
+        env::set_var(name, value);
+    }
+}
+
+fn find_opencc_lib_dirs() -> Vec<PathBuf> {
+    println!("cargo:rerun-if-env-changed=OPENCC_LIB_DIRS");
+    env::var("OPENCC_LIB_DIRS")
+        .map(|x| x.split(":").map(PathBuf::from).collect::<Vec<PathBuf>>())
+        .or_else(|_| Ok(vec![find_opencc_dir()?.join("lib")]))
+        .or_else(|_: env::VarError| -> Result<_, env::VarError> {
+            Ok(run_pkg_config().link_paths)
+        })
+        .expect("Couldn't find OpenCC library directory")
+}
+
+fn find_opencc_include_dirs() -> Vec<PathBuf> {
+    println!("cargo:rerun-if-env-changed=OPENCC_INCLUDE_DIRS");
+    env::var("OPENCC_INCLUDE_DIRS")
+        .map(|x| x.split(":").map(PathBuf::from).collect::<Vec<PathBuf>>())
+        .or_else(|_| Ok(vec![find_opencc_dir()?.join("include")]))
+        .or_else(|_: env::VarError| -> Result<_, env::VarError> {
+            Ok(run_pkg_config().include_paths)
+        })
+        .expect("Couldn't find OpenCC include directory")
+}
+
+fn find_opencc_dir() -> Result<PathBuf, env::VarError> {
+    println!("cargo:rerun-if-env-changed=OPENCC_DIR");
+    env::var("OPENCC_DIR")
+        .map(PathBuf::from)
+}
+
+fn determine_mode<T: AsRef<str>>(libdirs: &Vec<PathBuf>, libs: &[T]) -> &'static str {
+    println!("cargo:rerun-if-env-changed=OPENCC_STATIC");
+    let kind = env::var("OPENCC_STATIC").ok();
+    match kind.as_ref().map(|s| &s[..]) {
+        Some("0") => return "dylib",
+        Some(_) => return "static",
+        None => {}
+    }
+
+    let files = libdirs.into_iter().flat_map(|d| d.read_dir().unwrap())
+        .map(|e| e.unwrap())
+        .map(|e| e.file_name())
+        .filter_map(|e| e.into_string().ok())
+        .collect::<HashSet<_>>();
+    let can_static = libs.iter().all(|l| {
+        files.contains(&format!("lib{}.a", l.as_ref())) ||
+            files.contains(&format!("{}.lib", l.as_ref()))
+    });
+    let can_dylib = libs.iter().all(|l| {
+        files.contains(&format!("lib{}.so", l.as_ref())) ||
+            files.contains(&format!("{}.dll", l.as_ref())) ||
+            files.contains(&format!("lib{}.dylib", l.as_ref()))
+    });
+
+    match (can_static, can_dylib) {
+        (true, false) => return "static",
+        (false, true) => return "dylib",
+        (false, false) => {
+            panic!(
+                "OpenCC libdirs at `{:?}` do not contain the required files \
+                    to either statically or dynamically link OpenCC",
+                libdirs
+            );
+        }
+        (true, true) => {}
+    }
+
+    "dylib"
+}
+
+fn run_pkg_config() -> pkg_config::Library {
+    pkg_config::Config::new()
+        .cargo_metadata(false)
+        .atleast_version(MIN_VERSION)
+        .probe("opencc")
+        .unwrap();
+
+    if !Command::new("pkg-config")
+        .arg(format!("--max-version={}", MAX_VERSION))
+        .arg("opencc")
+        .status().unwrap().success() {
+        panic!(format!("OpenCC version must be no higher than {}", MAX_VERSION));
+    }
+
+    pkg_config::Config::new()
+        .cargo_metadata(false)
+        .probe("opencc").unwrap()
+}
---
+++ src/lib.rs
@@ -0,0 +1,168 @@
+extern crate libc;
+
+use libc::{c_int, size_t, c_void, c_char};
+use libc::wchar_t;
+
+use std::ffi::{CStr, CString};
+use std::mem::transmute;
+
+#[link(name = "opencc")]
+extern "C" {
+    pub fn opencc_open(config_file_path: *const c_char) -> *mut c_void;
+    pub fn opencc_open_w(config_file_path: *const wchar_t) -> *mut c_void;
+    pub fn opencc_close(opencc: *mut c_void) -> c_int;
+    pub fn opencc_convert_utf8(opencc: *mut c_void, input: *const c_char, length: size_t) -> *mut c_char;
+    pub fn opencc_convert_utf8_to_buffer(opencc: *mut c_void, input: *const c_char, length: size_t, output: *mut c_char) -> size_t;
+    pub fn opencc_convert_utf8_free(str: *mut c_char);
+    pub fn opencc_error() -> *const c_char;
+}
+
+#[cfg(not(feature = "no-default-config-file-names"))]
+/// Default config file names.
+pub mod default_config_file_paths {
+    /// Simplified Chinese to Traditional Chinese
+    pub const S2T: &'static str = "s2t.json";
+    /// Traditional Chinese to Simplified Chinese
+    pub const T2S: &'static str = "t2s.json";
+    /// Simplified Chinese to Traditional Chinese (Taiwan Standard)
+    pub const S2TW: &'static str = "s2tw.json";
+    /// Traditional Chinese (Taiwan Standard) to Simplified Chinese
+    pub const TW2S: &'static str = "tw2s.json";
+    /// Simplified Chinese to Traditional Chinese (Hong Kong Standard)
+    pub const S2HK: &'static str = "s2hk.json";
+    /// Traditional Chinese (Hong Kong Standard) to Simplified Chinese
+    pub const HK2S: &'static str = "hk2s.json";
+    /// Simplified Chinese to Traditional Chinese (Taiwan Standard) with Taiwanese idiom
+    pub const S2TWP: &'static str = "s2twp.json";
+    /// Traditional Chinese (Taiwan Standard) to Simplified Chinese with Mainland Chinese idiom
+    pub const TW2SP: &'static str = "tw2sp.json";
+    /// Traditional Chinese (OpenCC Standard) to Taiwan Standard
+    pub const T2TW: &'static str = "t2tw.json";
+    /// Traditional Chinese (OpenCC Standard) to Hong Kong Standard
+    pub const T2HK: &'static str = "t2hk.json";
+}
+
+pub struct OpenCC {
+    opencc: *mut c_void,
+}
+
+unsafe impl Send for OpenCC {}
+
+unsafe impl Sync for OpenCC {}
+
+impl OpenCC {
+    pub fn new(config_file_path: &str) -> OpenCC {
+        let config_file_path = CString::new(config_file_path).unwrap();
+
+        let opencc = unsafe {
+            opencc_open(config_file_path.as_ptr())
+        };
+
+        let v: size_t = unsafe {
+            transmute(opencc)
+        };
+        if v == !0 {
+            panic!("Cannot use this config file path.");
+        }
+
+        OpenCC {
+            opencc
+        }
+    }
+
+    pub fn convert(&self, input: &str) -> String {
+        let length = input.len();
+        let input = CString::new(input).unwrap();
+
+        let result_ptr = unsafe {
+            opencc_convert_utf8(self.opencc, input.as_ptr(), length)
+        };
+        let result_cstr = unsafe {
+            CStr::from_ptr(result_ptr)
+        };
+        let result = result_cstr.to_string_lossy().to_string();
+
+        unsafe {
+            opencc_convert_utf8_free(result_ptr);
+        }
+
+        result
+    }
+
+    pub fn convert_owned(&self, input: &str, output: String) -> String {
+        let length = input.len();
+        let input = CString::new(input).unwrap();
+
+        let mut output = output.into_bytes();
+        let o_len = output.len();
+
+        output.reserve(length * 2);
+
+        let input_ptr = unsafe {
+            output.as_ptr().add(output.len()) as *mut c_char
+        };
+
+        let size = unsafe {
+            opencc_convert_utf8_to_buffer(self.opencc, input.as_ptr(), length, input_ptr)
+        };
+
+        unsafe {
+            output.set_len(o_len + size);
+        }
+
+        unsafe {
+            String::from_utf8_unchecked(output)
+        }
+    }
+}
+
+impl Drop for OpenCC {
+    fn drop(&mut self) {
+        if !self.opencc.is_null() {
+            unsafe {
+                opencc_close(self.opencc);
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_tw2sp() {
+        let opencc = OpenCC::new(default_config_file_paths::TW2SP);
+        assert_eq!("凉风有讯，秋月无边，亏我思娇的情绪好比度日如年。虽然我不是玉树临风，潇洒倜傥，但我有广阔的胸襟，加强劲的臂弯。",
+                   &opencc.convert("涼風有訊，秋月無邊，虧我思嬌的情緒好比度日如年。雖然我不是玉樹臨風，瀟灑倜儻，但我有廣闊的胸襟，加強勁的臂彎。"));
+    }
+
+    #[test]
+    fn test_tw2sp_owned() {
+        let s = String::from("涼風有訊，秋月無邊，虧我思嬌的情緒好比度日如年。");
+
+        let opencc = OpenCC::new(default_config_file_paths::TW2SP);
+        let s = opencc.convert_owned("雖然我不是玉樹臨風，瀟灑倜儻，但我有廣闊的胸襟，加強勁的臂彎。", s);
+
+        assert_eq!("涼風有訊，秋月無邊，虧我思嬌的情緒好比度日如年。虽然我不是玉树临风，潇洒倜傥，但我有广阔的胸襟，加强劲的臂弯。",
+                   &s);
+    }
+
+    #[test]
+    fn test_s2twp() {
+        let opencc = OpenCC::new(default_config_file_paths::S2TWP);
+        assert_eq!("涼風有訊，秋月無邊，虧我思嬌的情緒好比度日如年。雖然我不是玉樹臨風，瀟灑倜儻，但我有廣闊的胸襟，加強勁的臂彎。",
+                   &opencc.convert("凉风有讯，秋月无边，亏我思娇的情绪好比度日如年。虽然我不是玉树临风，潇洒倜傥，但我有广阔的胸襟，加强劲的臂弯。"));
+    }
+
+    #[test]
+    fn test_s2twp_owned() {
+        let s = String::from("凉风有讯，秋月无边，亏我思娇的情绪好比度日如年。");
+
+        let opencc = OpenCC::new(default_config_file_paths::S2TWP);
+        let s = opencc.convert_owned("虽然我不是玉树临风，潇洒倜傥，但我有广阔的胸襟，加强劲的臂弯。", s);
+
+        assert_eq!("凉风有讯，秋月无边，亏我思娇的情绪好比度日如年。雖然我不是玉樹臨風，瀟灑倜儻，但我有廣闊的胸襟，加強勁的臂彎。",
+                   &s);
+    }
+}
